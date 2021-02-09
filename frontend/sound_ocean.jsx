6603d0e88c571b1955a6d45500f7d94b12452bf9
Edwin Cheng <edwincheng3@gmail.com>
2021-02-08 18:01:07 -0800

    Finished right before SessionForm Components

--- frontend/sound_ocean.jsx
+++ frontend/sound_ocean.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store"
 import Root from "./components/root"
 
-// testing
-// import { signup, login, logout } from "./actions/session_actions"
+// Testing
+import { signup, login, logout } from "./actions/session_actions"
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,11 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
 
     // Testing
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-    // window.login = login
-    // window.signup = signup
-    // window.logout = logout
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = login
+    window.signup = signup
+    window.logout = logout
 
     ReactDOM.render(<Root store={store} />, root);
 });
