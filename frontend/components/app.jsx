c60e47b1895760cb41d622437463668cc6db9617
Edwin Cheng <edwincheng3@gmail.com>
2021-02-08 17:03:04 -0800

    Finished phase: Updating the Entry Point

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>SoundOcean</h1>
+    </div>
+);
+
+export default App;
--- frontend/sound_ocean.jsx
+++ frontend/sound_ocean.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store"
+import Root from "./components/root"
 
-import { signup, login, logout } from './util/session_api_util'
+// testing
+// import { signup, login, logout } from "./actions/session_actions"
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    window.signup = signup
-    window.login = login
-    window.logout = logout
-
+    const store = configureStore();
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to SoundOcean</h1>, root);
+
+    // Testing
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
+    // window.login = login
+    // window.signup = signup
+    // window.logout = logout
+
+    ReactDOM.render(<Root store={store} />, root);
 });
