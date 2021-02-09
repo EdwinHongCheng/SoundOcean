4fe56c3213b52bbd8d1c3f4ab56ef3b887296ecd
Edwin Cheng <edwincheng3@gmail.com>
2021-02-09 11:58:13 -0800

    Finished Bootstrapping, Route Utils

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import {
     Route,
-    // prob have to import others later, use BenchBnB as Solution
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
 } from 'react-router-dom';
 
 import GreetingContainer from "./greeting/greeting_container";
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 // prob have to import others later, use BenchBnB as Solution
+
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
@@ -16,8 +21,10 @@ const App = () => (
             <GreetingContainer />
         </header>
 
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
     </div>
 );
 
