adbcd60f2cea1cba2a8970e4ee532f69726e79bd
Edwin Cheng <edwincheng3@gmail.com>
2021-02-08 17:31:42 -0800

    Adds greeting component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import GreetingContainer from "./greeting/greeting_container";
+
 const App = () => (
     <div>
-        <h1>SoundOcean</h1>
+        <header>
+            <h1>Bench BnB</h1>
+            <GreetingContainer />
+        </header>
     </div>
 );
 
