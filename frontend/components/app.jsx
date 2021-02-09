c57b912494043ba84eec1b14fa2a08af110f606b
Edwin Cheng <edwincheng3@gmail.com>
2021-02-09 09:39:56 -0800

    adds Session Form Components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import {
+    Route,
+    // prob have to import others later, use BenchBnB as Solution
+} from 'react-router-dom';
 
 import GreetingContainer from "./greeting/greeting_container";
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+// prob have to import others later, use BenchBnB as Solution
 
 const App = () => (
     <div>
         <header>
-            <h1>SoundOcean</h1>
+            <h1>SoundOcean (from app.jsx)</h1>
             <GreetingContainer />
         </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
