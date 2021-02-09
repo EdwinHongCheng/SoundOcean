711f537832f1ee7f4b4cd3caffc6124a9c25e374
Edwin Cheng <edwincheng3@gmail.com>
2021-02-09 09:28:19 -0800

    Rollbacked and put unique true in add index instead for users table

--- db/migrate/20210208220002_create_users.rb
+++ db/migrate/20210208220002_create_users.rb
@@ -1,15 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, null: false, unique: true
-      t.string :email, null: false, unique: true
+      t.string :username, null: false
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false, unique: true
 
       t.timestamps
     end
 
-    add_index :users, :username
-    add_index :users, :email
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 2021_02_08_220002) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["username"], name: "index_users_on_username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
