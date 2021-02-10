d52da7f47136d3c4d94cbd2f08b21c4dcf2f00af
Edwin Cheng <edwincheng3@gmail.com>
2021-02-10 12:23:04 -0800

    added dropdown button - need to make pretty next

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -11,14 +11,36 @@ const Greeting = ({ currentUser, logout, openModal }) => {
         </>
     );
 
+    // DropDown Test
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    const myFunction = () => {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
     const personalGreeting = () => (
         <>
             <hgroup className="header-group">
-                
+
                 {/* [TEST] DropDown Button - from w3schools: How TO - Clickable Dropdown  */}
-                <div class="dropdown">
-                    <button class="dropbtn">Hi, {currentUser.username}!</button>
-                    <div id="myDropdown" class="dropdown-content">
+                <div className="dropdown">
+                    <button onClick={myFunction} className="dropbtn">Hi, {currentUser.username}!</button>
+                    <div id="myDropdown" className="dropdown-content">
 
                         <button className="header-button" onClick={logout}>Sign out</button>
 
