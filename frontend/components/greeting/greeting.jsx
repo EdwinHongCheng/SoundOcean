3c62296e7a01fff859950b6f24fcc67a75c07770
Edwin Cheng <edwincheng3@gmail.com>
2021-02-10 02:39:52 -0800

    added h2 to sessionLinks (in greeting.jsx)

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout, openModal }) => {
     const sessionLinks = () => (
-        <nav className="login-signup">
-            <button onClick={() => openModal('login')}>Sign in</button>
-            <button onClick={() => openModal('signup')}>Create account</button>
-        </nav>
+        <>
+            <h2>~ where SoundWaves never end (◠‿◠✿) ~</h2>
+            <nav className="login-signup">
+                <button onClick={() => openModal('login')}>Sign in</button>
+                <button onClick={() => openModal('signup')}>Create account</button>
+            </nav>
+        </>
     );
 
     const personalGreeting = () => (
-        <hgroup className="header-group">
-            <h2 className="header-name">Hi, {currentUser.username}!</h2>
-            <button className="header-button" onClick={logout}>Sign out</button>
-        </hgroup>
+        <>
+            <hgroup className="header-group">
+                <h2 className="header-name">Hi, {currentUser.username}!</h2>
+                <button className="header-button" onClick={logout}>Sign out</button>
+            </hgroup>
+        </>
     );
 
     return currentUser ? personalGreeting() : sessionLinks();
