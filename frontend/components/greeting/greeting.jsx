64a77afd219591212d6a9f3d28263d015832b01f
Edwin Cheng <edwincheng3@gmail.com>
2021-02-11 14:42:07 -0800

    working on dropdown, looking better

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -13,14 +13,11 @@ const Greeting = ({ currentUser, logout, openModal }) => {
 
     const sessionLinks = () => (
         <div className="signin-navbar">
-
             <div className="navbar-login-left-sect">
                 {soLogo}
                 <p className="soundOcean-greeting-text">SoundOcean</p>
             </div>
             
-
-            {/* NavBar Right - Unordered List of Buttons/Links */}
             <ul className="navbar-login-right-sect">
                 <li>
                     <button className="signin-button-navbar" onClick={() => openModal('login')}>Sign in</button>
@@ -54,35 +51,25 @@ const Greeting = ({ currentUser, logout, openModal }) => {
     }
 
     const personalGreeting = () => (
-        <>
-            <hgroup className="header-group">
+        <ul className="navbar-post-signin">
+            {soLogo}
+            <li><a className="home-button" href="#">Home</a></li>
+            <li><a className="nav-button" href="#">Stream</a></li>
+            <li><a className="nav-button" href="#">Library</a></li>
+            <input className="searchBar" type="input" placeholder="Search" />
+            <li><a className="upload-button" href="#">Upload</a></li>
+            <li><a className="nav-button" href="#">{currentUser.username}</a></li>
+            <li><a className="nav-button" href="#">GitHub</a></li>
+            <li><a className="nav-button" href="#">LinkedIn</a></li>
 
-                <ul className="navbar-post-signin">
-                    {soLogo}
-                    <li><a className="nav-button" href="#">Home</a></li>
-                    <li><a className="nav-button" href="#">Stream</a></li>
-                    <li><a className="nav-button" href="#">Library</a></li>
-                    <li><input type="input" placeholder="Search for artists, bands, tracks, podcases" /></li>
-                    <li><a className="nav-button" href="#">Upload</a></li>
-
-                    {/* [TEST] DropDown Button - from w3schools: How TO - Clickable Dropdown  */}
-                    <li><a className="nav-button" href="#">{currentUser.username}</a></li>
-
-                    <li><a className="nav-button" href="#">GitHub</a></li>
-                    <li><a className="nav-button" href="#">LinkedIn</a></li>
-
-                    <div className="dropdown">
-                        <button onClick={dropdownFunc} className="dropbtn">&#8230;</button>
-                        <div id="myDropdown" className="dropdown-content">
-                            <button className="header-button" onClick={logout}>Sign out</button>
-                        </div>
-                    </div>
-                </ul>
-
-
-
-            </hgroup>
-        </>
+            {/* [TEST] DropDown Button - from w3schools: How TO - Clickable Dropdown  */}
+            <div className="dropdown">
+                <button onClick={dropdownFunc} className="dropbtn">&#8230;</button>
+                <div id="myDropdown" className="dropdown-content">
+                    <div onClick={logout}>Sign out</div>
+                </div>
+            </div>
+        </ul>
     );
 
     return currentUser ? personalGreeting() : sessionLinks();
