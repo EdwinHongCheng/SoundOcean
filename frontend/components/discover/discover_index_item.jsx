b1b1dfdd359dea84ed3c4d37a0e550546b282ed7
Edwin Cheng <edwincheng3@gmail.com>
2021-02-15 20:24:05 -0800

    shrunk user text in discover page

--- frontend/components/discover/discover_index_item.jsx
+++ frontend/components/discover/discover_index_item.jsx
@@ -10,11 +10,8 @@ class DiscoverIndexItem extends React.Component {
             <div className="discoverIndexItem">
                 <Link to={`/tracks/${currentTrack.id}`}><img src={currentTrack.imageURL} className="coverArt" /></Link>
                 <br />
-                <p>{currentTrack.id}. Title: {currentTrack.title}</p>
-                <p>- Created by: User {currentTrack.creator_id}</p>
-                <Link to={`/tracks/${currentTrack.id}`}>- Link to This Track</Link>
-                <br />
-                <br />
+                <p className="creatorNameText">User {currentTrack.creator_id}</p>
+                <Link to={`/tracks/${currentTrack.id}`}><p>{currentTrack.title}</p></Link>
                 <br />
             </div>
         )
