34b2678b30e8f32fa06cfba9e43343b9cdd02cd4
Edwin Cheng <edwincheng3@gmail.com>
2021-02-16 18:21:47 -0800

    testing play pause buttons

--- frontend/components/playbar/playbar.jsx
+++ frontend/components/playbar/playbar.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
 
 class PlayBar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
 
     render() {
-        let playbar;
+        let playbarAll;
 
         if (this.props.currentTrack) {
 
-            playbar = (
+            let playbar = (
+                <audio id="audio" controls autoPlay loop key={this.props.currentTrack.id}>
+                    <source src={this.props.currentTrack.audioURL} type="audio/mpeg" />
+                    <source src={this.props.currentTrack.audioURL} type="audio/ogg" />
+                                Your browser does not support the audio tag.
+                </audio>
+            )
+
+            // [TEST] Play/Pause Button
+
+            playbarAll = (
                 <div className="playbar">
                     {/* NOTE: need a unique "key" to tell React it updated !!! */}
 
                     <div className="playbar-left">
-                        <audio controls autoPlay loop key={this.props.currentTrack.id}>
-                            <source src={this.props.currentTrack.audioURL} type="audio/mpeg" />
-                            <source src={this.props.currentTrack.audioURL} type="audio/ogg" />
-                                Your browser does not support the audio tag.
-                        </audio>
+                        {playbar}
                     </div>
 
                     <p className="playbar-right">Now Playing: {this.props.currentTrack.title}</p>
+
+
+                    {/* [TEST] Play/Pause Buttons (delete later) */}
+                    <button onClick={() => document.getElementById('audio').play()}>Play Button</button>
+                    <button onClick={() => document.getElementById('audio').pause()}>Pause Button</button>
                 </div>
             )
         }
 
         return (
             <div>
-                {playbar}
+                {playbarAll}
             </div>
         )
     }
