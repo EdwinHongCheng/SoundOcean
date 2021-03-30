78f1b083b984b9d953149e430979325cbc556bb8
Edwin Cheng <edwincheng3@gmail.com>
2021-03-30 13:53:48 -0700

    add colored margin for navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -63,51 +63,53 @@ const Navbar = ({ currentUser, logout }) => {
 
     if (currentUser) {
         navigationBar = (
-    
-            <nav className="navigation-bar">
 
-                <div className="navbar-left">
-                    {soLogo}
-                    <Link to="/discover" className="home-button">Home</Link>
-                    <Link to="/stream"className="nav-button" >Stream</ Link>
-                    <Link to="/library" className="nav-button" >Library</ Link>
-                </div>
+            <div className="loggedin-navbar-parent">
+                <nav className="navigation-bar">
 
-                {/* <input className="searchBar" type="input" placeholder="Search" /> */}
+                    <div className="navbar-left">
+                        {soLogo}
+                        <Link to="/discover" className="home-button">Home</Link>
+                        <Link to="/stream"className="nav-button" >Stream</ Link>
+                        <Link to="/library" className="nav-button" >Library</ Link>
+                    </div>
 
-                <div className="navbar-right">
-                    <Link to="/upload" className="upload-button">Upload</Link>
+                    {/* <input className="searchBar" type="input" placeholder="Search" /> */}
 
-                    <Link to={`/users/${currentUser.id}`} className="prof-pic-parent">
-                        <img className="mini-prof-pic" src={currentUser.profilePicURL} />
-                    </Link>
-                    
-                    
-                    {/* DropDown Button 2 (for User) */}
+                    <div className="navbar-right">
+                        <Link to="/upload" className="upload-button">Upload</Link>
 
-                    <div className="dropdown2">
-                        <p onClick={dropdownFunc2} className="dropbtn2">{currentUser.username}</p>
-                        <div id="myDropdown2" className="dropdown-content2">
-                            <Link to={`/users/${currentUser.id}`}>
-                                <div>Profile</div>
-                            </Link>
+                        <Link to={`/users/${currentUser.id}`} className="prof-pic-parent">
+                            <img className="mini-prof-pic" src={currentUser.profilePicURL} />
+                        </Link>
+                        
+                        
+                        {/* DropDown Button 2 (for User) */}
+
+                        <div className="dropdown2">
+                            <p onClick={dropdownFunc2} className="dropbtn2">{currentUser.username}</p>
+                            <div id="myDropdown2" className="dropdown-content2">
+                                <Link to={`/users/${currentUser.id}`}>
+                                    <div>Profile</div>
+                                </Link>
+                            </div>
+                        </div>
+
+
+                        <a className="nav-button" href="https://github.com/EdwinHongCheng/SoundOcean" target="_blank">GitHub</a>
+                        <a className="nav-button" href="https://www.linkedin.com/in/edwin-cheng-a603819b/" target="_blank">LinkedIn</a>
+
+                        {/* DropDown Button for "..." (w3schools): How TO - Clickable Dropdown  */}
+                        <div className="dropdown">
+                            <p onClick={dropdownFunc} className="dropbtn nav-button">...</p>
+                            <div id="myDropdown" className="dropdown-content">
+                                <div onClick={logout}>Sign out</div>
+                            </div>
                         </div>
                     </div>
 
-
-                    <a className="nav-button" href="https://github.com/EdwinHongCheng/SoundOcean" target="_blank">GitHub</a>
-                    <a className="nav-button" href="https://www.linkedin.com/in/edwin-cheng-a603819b/" target="_blank">LinkedIn</a>
-
-                    {/* DropDown Button for "..." (w3schools): How TO - Clickable Dropdown  */}
-                    <div className="dropdown">
-                        <p onClick={dropdownFunc} className="dropbtn nav-button">...</p>
-                        <div id="myDropdown" className="dropdown-content">
-                            <div onClick={logout}>Sign out</div>
-                        </div>
-                    </div>
-                </div>
-
-            </nav>
+                </nav>
+            </div>
         );
     }
     
