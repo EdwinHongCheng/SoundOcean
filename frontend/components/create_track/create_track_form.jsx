b93648b2b2f87af5e3b340408dc2ddb50ce65e5a
Edwin Cheng <edwincheng3@gmail.com>
2021-04-06 18:05:09 -0700

    upload page - upload cover art preview + button

--- frontend/components/create_track/create_track_form.jsx
+++ frontend/components/create_track/create_track_form.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavbarContainer from "../navbar/navbar_container"
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
 class CreateTrackForm extends React.Component {
     constructor(props) {
@@ -96,7 +98,9 @@ class CreateTrackForm extends React.Component {
         let imagePreview = null;
         if (this.state.coverArtPreviewURL) {
             imagePreview = (
-                <img src={this.state.coverArtPreviewURL} className="previewArt"/>
+                <div className="image-preview-parent">
+                    <img src={this.state.coverArtPreviewURL} className="upload-preview-art"/>
+                </div>
             )
         }
 
@@ -121,39 +125,54 @@ class CreateTrackForm extends React.Component {
                 <div>
                     {/* Audio Upload (Update) */}
                     <div className="replace-track-parent">
-                        <p className="replace-track-left">Replace Current Audio Track</p>
-
-                            <label className="replace-track-right">
-                                Replace File
-                                <input className="replace-track-input"
-                                    type="file"
-                                    onChange={this.handleAudioFile}
-                                /> 
-                            </label>
-
-                    </div>
-
-                    <div className="upload-form-parent">
-                        <form onSubmit={this.handleSubmit}>
-                            {this.renderErrors()}
-                            <h1>Create a New Track</h1>
-                            <label>Title
-                                    <input
-                                    type="text"
-                                    value={this.state.title}
-                                    onChange={this.update('title')}
-                                />
-                            </label>
-                            {/* Cover Art Preview */}
-                            {imagePreview}
-                            <p>[Optional] Upload Cover Art</p>
-                            <input 
+                        <p className="replace-track-left">Replace the current audio track</p>
+                        <label className="replace-track-right">Replace File
+                            <input className="replace-track-input"
                                 type="file"
-                                onChange={this.handleFile}
-                            />           
-                            <input type="submit" value="Create Track" />
-                        </form>
+                                onChange={this.handleAudioFile}
+                            /> 
+                        </label>
                     </div>
+
+                    <div className="form-info-parent-parent">
+                        <div className="form-info-parent">
+
+                            <div className="form-info-tab-parent">
+                                <p className="form-info-tab">Basic info</p>
+                            </div>
+
+                            <div className="form-info">
+                                {/* Cover Art Preview */}
+                                <div className="image-preview-and-input">
+                                    <div className="image-preview-parent">
+                                        {imagePreview}
+                                    </div>
+                                    {/* Upload Cover Art */}
+                                    <label className="upload-art-and-input-label">
+                                        <FontAwesomeIcon id="camera-icon" icon={faCamera}/>
+                                        Upload image
+                                        <input type="file" className="upload-art-input"
+                                            onChange={this.handleFile}
+                                        />       
+                                    </label>
+                                </div>
+
+                                <form onSubmit={this.handleSubmit}>
+                                    {this.renderErrors()}
+                                    <label>Title
+                                            <input
+                                            type="text"
+                                            value={this.state.title}
+                                            onChange={this.update('title')}
+                                        />
+                                    </label>
+                                    <input type="submit" value="Create Track" />
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
 
             )
