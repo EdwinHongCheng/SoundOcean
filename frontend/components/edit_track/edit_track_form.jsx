b48eaa9407fadb7cd60a8a49a1efcc1f327b54e9
Edwin Cheng <edwincheng3@gmail.com>
2021-04-08 17:32:41 -0700

    edit track form wip v2

--- frontend/components/edit_track/edit_track_form.jsx
+++ frontend/components/edit_track/edit_track_form.jsx
@@ -57,12 +57,9 @@ class EditTrackForm extends React.Component {
         return (
             <ul>
                 {this.props.errors.map((error, i) => (
-                    <>
-                        <li key={`error-${i}`} className="renderedErrors">
-                            {error}
-                        </li>
-                        <br />
-                    </>
+                    <li key={`error-${i}`} className="renderedErrors">
+                        {error}
+                    </li>
                 ))}
             </ul>
         );
