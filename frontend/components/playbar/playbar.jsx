a73bf8bd51e335f6b3e4008f91a441f714923284
Edwin Cheng <edwincheng3@gmail.com>
2021-05-07 17:06:22 -0700

    playbar - replaced linkedin link w portfolio site

--- frontend/components/playbar/playbar.jsx
+++ frontend/components/playbar/playbar.jsx
@@ -272,7 +272,7 @@ class PlayBar extends React.Component {
                                     </Link>
                                 </div>
 
-                                <a className="playbar-right-blurb" href="https://www.linkedin.com/in/edwin-cheng-a603819b/" target="_blank">
+                                <a className="playbar-right-blurb" href="https://edwinhongcheng.github.io" target="_blank">
                                     Site created by Edwin Cheng
                                 </a>
                             </div>
