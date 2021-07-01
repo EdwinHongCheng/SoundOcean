9880a6eb6250558f903d3ff97adfda216fab0500
Edwin Cheng <edwincheng3@gmail.com>
2021-07-01 00:43:26 -0700

    code cleanup

--- frontend/components/homepage/homepage_index_item.jsx
+++ frontend/components/homepage/homepage_index_item.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class HomepageIndexItem extends React.Component {
     render() {
@@ -7,12 +6,10 @@ class HomepageIndexItem extends React.Component {
 
         let currentCover;
         if ((this.props.idx + 1) % 6 === 0) {
-            // currentCover = (<Link to={`/tracks/${currentTrack.id}`}><img src={currentTrack.imageURL} className="homepageCoverArtLast" /></Link>)
             currentCover = (<img className="homepageCoverArtLast"
                     onClick={() => this.props.openModal('login')} 
                     src={currentTrack.imageURL} />)
         } else {
-            // currentCover = (<Link to={`/tracks/${currentTrack.id}`}><img src={currentTrack.imageURL} className="homepageCoverArt" /></Link>)
             currentCover = (<img className="homepageCoverArt"
                     onClick={() => this.props.openModal('login')} 
                     src={currentTrack.imageURL} />)
@@ -22,12 +19,10 @@ class HomepageIndexItem extends React.Component {
             <div className="homepageIndexItem">
                 {currentCover}
 
-                {/* <Link to={`/tracks/${currentTrack.id}`}><p className="homepageTrackText">{currentTrack.title}</p></Link> */}
-                <div onClick={() => this.props.openModal('login')}><p className="homepageTrackText">{currentTrack.title}</p></div>
+                <div onClick={() => this.props.openModal('login')}>
+                    <p className="homepageTrackText">{currentTrack.title}</p>
+                </div>
 
-                {/* <Link to={`/users/${currentTrack.creator_id}`}>
-                    <div className="homepageCreatorNameText">{currentTrack.creator}</div>
-                </Link> */}
                 <div onClick={() => this.props.openModal('login')}>
                     <div className="homepageCreatorNameText">{currentTrack.creator}</div>
                 </div>
